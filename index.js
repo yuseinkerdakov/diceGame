846ac929ba5d268f924c94dd573f5ab3f68dc6f2
Yusein Kerdakov <108539299+yuseinkerdakov@users.noreply.github.com>
2022-07-06 20:59:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let random1 = Math.floor(Math.random() * 6) + 1;
+let random2 = Math.floor(Math.random() * 6) + 1;
+
+let image1 = 'images/dice' + random1 + '.png';
+let image2 = 'images/dice' + random2 + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src', image1);
+document.querySelectorAll('img')[1].setAttribute('src', image2);
+
+
+if (window.performance.navigation.type > 0) {
+    if (random1 > random2) {
+        document.querySelector('h1').innerHTML = '&#128681 Player 1 Wins!' 
+    } else if (random1 < random2) {
+        document.querySelector('h1').innerHTML = 'Player 2 Wins! &#128681' 
+    } else {
+        document.querySelector('h1').innerHTML = 'Draw!' 
+    }
+}
